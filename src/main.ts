7d0a989c43e0c49aa8041e5d06e4af1734f9205f
Ojage <hismajestylordkratos@gmail.com>
2023-04-17 16:56:28 +0000

    Added Allow Control-allow-origin acces to from mologo-client.com

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use((req: any, res: { setHeader: (arg0: string, arg1: string) => void; }, next: () => void) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://mologo-client.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
